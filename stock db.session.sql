f8e50d57c21aaa448d2fa955659b6ec9f572699e
sant1ag0x00 <75492715+sant1ag0x00@users.noreply.github.com>
2022-11-21 21:00:46 +0100

    Delete stock db.session.sql

--- stock db.session.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.id,
-    s.Sector,
-    s.Name
-    FROM User u
-    INNER JOIN user_stock AS us ON u.id = us.user_id
-    INNER JOIN stocks s on us.stock_symbol = s.Symbol
-WHERE u.name = 'Robert'
